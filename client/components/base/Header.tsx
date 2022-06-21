3710b94f331143972ad4df99d0c1754f152f6944
Alifhossain2543 <alif.pab120na.12@gmail.com>
2022-06-21 10:46:57 +0600

    added bootstrap customization

---
+++ client/components/base/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+      <div className="border min-h-50">
+        <div className="border  w-100 border-somecolor flex flex-row d-flex align-items-center justify-content-between ">
+          <h5>Micro-BLOG</h5>
+
+          <div className="px-5">
+            <h6>Buttons will be here</h6>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import '../scss/custom.scss'
+import type { AppProps } from "next/app"
+import Header from "../components/base/Header"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
