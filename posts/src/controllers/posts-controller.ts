bab2968cd98f61a0444fe332a92a498b83ad056c
Alifhossain2543 <alif.pab120na.12@gmail.com>
2022-06-26 01:26:07 +0600

    worked on posts apis

--- posts/src/controllers/posts-controller.ts
+++ posts/src/controllers/posts-controller.ts
@@ -85,3 +85,85 @@ export const createPost = async (req: Request, res: Response) => {
     res.status(201).json({ status: "success", post: newPost })
 
 }
+
+export const updatePost = async (req: Request, res: Response) => {
+  const postId = parseInt(req.params.id)
+  const currentUser = req.currentUser
+
+  //check if the author is in the post db
+  const user = await prismaClient.user.findUnique({where : {userId : parseInt(currentUser!.id!)}})
+
+  //throw an error if the author dosen't exist
+  if(!user){
+    throw new BadRequest("You are not authorized to update this post.")
+  }
+  //check if the post exists
+  let post;
+  post = await prismaClient.post.findUnique({
+    where: {
+      id: postId,
+    },
+    include : {
+      author : true
+    }
+  })
+
+  if (!post) {
+    throw new BadRequest("Post not found.")
+  }
+
+  //throw an error if the post dosen't exist
+
+  //  -------------update the post-----------------
+
+  //updated the title if provided
+  if (req.body.title) {
+    post = await prismaClient.post.update({
+      where: { id: postId },
+      data: { title: req.body.title },
+      include: {
+        author: true,
+      },
+    })
+  }
+
+  //updated the content if provided
+   if (req.body.content) {
+     post = await prismaClient.post.update({
+       where: { id: postId },
+       data: { content: req.body.content },
+       include: {
+         author: true,
+       },
+     })
+   }
+
+  //send the post back to the client
+  res.status(200).json({ status: "success", post: post })
+}
+
+export const deletePost = async (req: Request, res: Response) => {
+  const postId = parseInt(req.params.id)
+  //check if the author is in the post db
+  const currentUser = req.currentUser
+
+  //check if the author match with the post author
+  const user = await prismaClient.user.findUnique({where : {userId : parseInt(currentUser!.id!)}})
+
+  //throw error if dosent't match or found
+
+  if (!user || user.userId != parseInt(req.currentUser!.id)) {
+    throw new BadRequest("You are not authorized to delete this post.")
+  } 
+
+  //delete the post
+
+  await prismaClient.post.delete({where : {
+    id : postId
+  }})
+
+
+  //send operation status to the client
+  res.status(200).json({ status: "Delete successful." })
+
+}
--- posts/src/routes/posts-route.ts
+++ posts/src/routes/posts-route.ts
@@ -1,7 +1,9 @@
 import {  Router } from "express"
 import {
   postTestRoute,
-  createPost
+  createPost,
+  updatePost,
+  deletePost,
 } from "../controllers/posts-controller"
 import { authUser} from '@hrioymahmud/blogcommon'
 
@@ -10,5 +12,7 @@ import { authUser} from '@hrioymahmud/blogcommon'
 const router = Router()
 router.get("/test", postTestRoute)
 router.post("/create", authUser, createPost)
+router.post("/update/:id", authUser, updatePost)
+router.delete("/delete/:id", authUser, deletePost)
 
 export { router as PostRouter}
