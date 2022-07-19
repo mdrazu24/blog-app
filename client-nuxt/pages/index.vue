327fb1c8f6760c66cfe8a9802c4cc7ad6203618b
Hridoy Mahmud <alif.pab120na.12@gmail.com>
2022-07-20 00:17:41 +0600

    added nuxt/vue client

---
+++ client-nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      message: "Hello Vue!"
+    }
+  },
+}
+</script>
